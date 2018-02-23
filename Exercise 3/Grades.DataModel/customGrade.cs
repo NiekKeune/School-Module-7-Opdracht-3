2c222a0cef9ac756cd2b0edbf4576618d85b1143
Niek Keunegit config -- global user.email "Niek.Keune@hva.nl"git clone git@gitlab.dmci.hva.nl:info/git4bit.gitgit config --global user.name "Niek Keune <niekkeune@gmail.com>
2018-02-23 14:58:53 +0100

    School Module 7 Opdracht 3

---
+++ Exercise 3/Grades.DataModel/customGrade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Grades.DataModel
+{
+    public partial class Grade
+    {
+        public bool ValidateAssessmentDate(DateTime assessmentDate)
+        {
+            if (assessmentDate > DateTime.Now)
+            {
+                throw new ArgumentOutOfRangeException("AssessmentDate", "The inserted date is later than the current date, and could therefore not be added.");
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool ValidateAssessmentGrade(string assessment)
+        {
+            Match matchGrade = Regex.Match(assessment, @"^[A-E][+-]?$");        //Checks if the current string contains any character between A-E (with + and - also being acceptable) and if the character does match, it'll return true.
+
+            if (matchGrade.Success == false)
+            {
+                throw new ArgumentOutOfRangeException("AssessmentGrade", "The inserted grade falls outside of the range of the possible grades, and could therefore not be added.");
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Exercise 3/Grades.DataModel/customTeacher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.DataModel
+{
+    public partial class Teacher
+    {
+        private const int MAX_CLASS_SIZE = 8;
+
+        public void EnrollInClass(Student student)
+        {
+            var students = (from s in Students
+                            where s.TeacherUserId == UserId
+                            select s).Count();
+
+            if (students >= MAX_CLASS_SIZE)
+            {
+                throw new ClassFullException("The student couldn't be added because the class is already full.", Class);
+            }
+
+            if (student.TeacherUserId == null)          //If the student has no TeacherID and therefore no Teacher, the Student's TeacherID will be set to the current Teacher who is adding said student.
+            {
+                student.TeacherUserId = UserId;
+            }
+            else
+            {
+                throw new ArgumentException("The student is already assigned to a class!");
+            }
+        }
+    }
+}
